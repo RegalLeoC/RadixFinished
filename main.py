9efe988eee9d3ce55462884dd23590c0dce52b1b
Victor Eduardo Garcia Sardaneta <regulusleoc@gmail.com>
2022-03-15 23:22:35 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from dtslib import *
+import random
+
+test = [121, 432, 564, 23, 23, 234, 1, 1, 45, 788, 1562]
+
+print("*****Lista Original*****")
+print(f"{test}")
+
+print(f"{radix_sort(test)}")
